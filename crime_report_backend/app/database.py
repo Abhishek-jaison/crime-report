a49f9e9e253654d3fbf61d5564eea3df636ab3bf
Abhishek-jaison <abhishekjaison04@gmail.com>
2026-02-14 16:30:50 +0530

    updated db

--- crime_report_backend/app/database.py
+++ crime_report_backend/app/database.py
@@ -2,10 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./crime_report.db"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Use env var or fallback to sqlite
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./crime_report.db")
+
+connect_args = {}
+if "sqlite" in SQLALCHEMY_DATABASE_URL:
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
